9398e3146b7ceb4b93098425ec6e12808b8c35c3
MarkoKajtazi <marko.kajtazi@students.finki.ukim.mk>
2024-11-14 21:17:00 +0100

    Update mse_scraper.py
    optimized and added so every issuer code is up to date if the last available date isn't today's date

--- mse_scraper.py
+++ mse_scraper.py
@@ -1,16 +1,15 @@
+import os.path
 from _pydatetime import timedelta
-
+from concurrent.futures import ThreadPoolExecutor
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
 import time
-import threading
 
-base_url = "https://www.mse.mk/mk/issuers/free-market"
-url_history = 'https://www.mse.mk/mk/stats/symbolhistory/'
+pd.set_option('display.max_columns', None)
 
-def fetch_issuers_names():
+def fetch_issuers_names(base_url, url_history):
     response = requests.get(base_url)
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
@@ -50,10 +49,6 @@ def fetch_issuer_data(code, from_date, to_date):
 
     return matrix
 
-def last_available_date(code):
-    data_frame = pd.read_csv("data/data_frame_" + code + ".csv")
-    last_date = pd.to_datetime(data_frame['Датум'], format='%m/%d/%Y').max()
-    print(last_date)
 
 def fetch_issuers_history_sync(issuers_codes):
     current_date = datetime.now()
@@ -69,18 +64,67 @@ def fetch_issuers_history_sync(issuers_codes):
         columns = ['Шифра на фирма', 'Датум', 'Цена на последна трансакција', 'Мак.', 'Мин.', 'Просечна цена', '%пром.', 'Количина', 'Промет во БЕСТ во денари', 'Вкупен промет во денари']
         data_frame = pd.DataFrame(matrix, columns=columns)
         data_frame.to_csv("data/data_frame_" + code + ".csv", index=False)
-        print(code)
-        print(data_frame)
 
-def is_empty_frame(issuers_codes):
-    for code in issuers_codes:
-        df = pd.read_csv("data/data_frame_" + code + ".csv")
-        if df.empty:
-            fetch_issuers_history_sync([code])
+        print(f"Data for {code} saved with {data_frame.shape[0]} rows")
 
-start_time = time.time()
-codes = fetch_issuers_names()
-fetch_issuers_history_sync(codes)
-is_empty_frame(codes)
-end_time = time.time()
-print(start_time, end_time, (end_time - start_time) / 60)
+def fetch_issuers_history_threads(issuers_codes):
+    current_date = datetime.now()
+
+    def process_issuer(code):
+        from_date = current_date - timedelta(days=365 * 10)
+        matrix = []
+
+        while from_date < current_date:
+            to_date = from_date + timedelta(days=365)
+            matrix.extend(fetch_issuer_data(code, from_date, to_date))
+            from_date = to_date
+
+        columns = ['Шифра на фирма', 'Датум', 'Цена на последна трансакција', 'Мак.', 'Мин.', 'Просечна цена', '%пром.', 'Количина', 'Промет во БЕСТ во денари', 'Вкупен промет во денари']
+        data_frame = pd.DataFrame(matrix, columns=columns)
+        data_frame.to_csv("data/data_frame_" + code + ".csv", index=False)
+        print(f"Data for {code} saved with {data_frame.shape[0]} rows")
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(process_issuer, issuers_codes)
+
+
+def get_last_available_date(issuer_code):
+    file_path = "data/data_frame_" + issuer_code + ".csv"
+    if not os.path.exists(file_path):
+        return None
+
+    df = pd.read_csv(file_path)
+    df["Датум"] = pd.to_datetime(df["Датум"], format="%d.%m.%Y")
+    available_date = df['Датум'].max().date()
+
+    return available_date
+
+
+def check_empty_frame(issuer_codes):
+    empty_codes = []
+    for issuer_code in issuer_codes:
+        last_available = get_last_available_date(issuer_code)
+        if last_available != datetime.today().date() or last_available is None:
+            empty_codes.append(issuer_code)
+
+    fetch_issuers_history_threads(empty_codes)
+
+def main():
+    base_url = "https://www.mse.mk/mk/issuers/free-market"
+    url_history = 'https://www.mse.mk/mk/stats/symbolhistory/'
+
+    codes = fetch_issuers_names(base_url, url_history)
+
+    if not os.path.exists("data"):
+        os.mkdir("data")
+        fetch_issuers_history_threads(codes)
+    else:
+        check_empty_frame(codes)
+
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    end_time = time.time()
+    print(start_time, end_time, (end_time - start_time) / 60)
